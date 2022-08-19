278df8bfc7fbc6a57ed82fc5b7b3594c97404920
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-08-19 13:28:16 -0300

    Initial Commit

---
+++ CoreBT/CoreBTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBTApp.swift
+//  CoreBT
+//
+//  Created by Wilsley Germano on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreBTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
