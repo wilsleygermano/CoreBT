9e5ea45c761f1d2419e3f353edc537523d423ca8
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-08-19 14:03:50 -0300

    Firs commit

--- CoreBT/ContentView.swift
+++ CoreBT/ContentView.swift
@@ -6,11 +6,47 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+
+class BluetoothViewModel: NSObject, ObservableObject {
+    private var centralManager: CBCentralManager?
+    private var periphals: [CBPeripheral] = []
+    @Published var peripheralNames: [String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !periphals.contains(peripheral) {
+            self.periphals.append(peripheral)
+            self.peripheralNames.append(peripheral.name ?? "Unnamed device")
+        }
+    }
+}
+
+
+
 
 struct ContentView: View {
+    @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(bluetoothViewModel.peripheralNames, id: \.self) { peripheral in Text(peripheral)
+            }
+            .navigationTitle("Peripherals")
+        }
     }
 }
 
